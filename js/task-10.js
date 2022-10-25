93b139e0d0f62a094544ddee32e19e445ad4ba57
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2022-10-25 13:59:08 +0300

    Intermedia version Task #10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,47 @@
+const inputNumberOfBlocksRef = document.querySelector("input");
+const onClickCreateBlocksRef = document.querySelector('button[data-create]');
+const onClickRemoveBlocksRef = document.querySelector('button[data-destroy]');
+const placeForBoxList = document.querySelector("#boxes");
+
+inputNumberOfBlocksRef.addEventListener('input', obtainInputValue);
+onClickCreateBlocksRef.addEventListener('click', onClickCreateBlocks);
+onClickRemoveBlocksRef.addEventListener('click', onClickRemoveBlocks);
+
+let numberOfElements = 0;
+
+function obtainInputValue(evt) {
+  numberOfElements = evt.currentTarget.value;
+}
+
+// Create box`s elements
+function onClickCreateBlocks() {
+  (numberOfElements === 0 || numberOfElements < 0) ? alert("Must be more '0'") : createBoxes(numberOfElements);  
+}
+
+placeForBoxList.style.display = 'flex';
+placeForBoxList.style.gap = '15px';
+placeForBoxList.style.flexWrap = "wrap";
+
+function createBoxes(amount) {
+  let html = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    let code = `<div style="height: ${size}px; width: ${size}px; background-color: ${getRandomHexColor()}"></div>`;
+    size += 10;
+    html.push(code)
+  }
+  placeForBoxList.insertAdjacentHTML("beforeend", html.join(""));
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+// Remove box`s elements
+function onClickCreateBlocks() {
+  (numberOfElements === 0 || numberOfElements < 0) ? alert("Must be more '0'") : createBoxes(numberOfElements);  
+}
+
+destroyBoxes()
+
